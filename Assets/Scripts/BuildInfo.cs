58be79b71b1cd9e421b539169a9b3d408b577c63
Windows10V1 <davidvenicu1@gmail.com>
2026-04-19 23:21:33 -0300

    hyhyh

--- Assets/Scripts/BuildInfo.cs
+++ Assets/Scripts/BuildInfo.cs
@@ -1,5 +1,5 @@
 using System;
 
 public static class BuildInfo {
-	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-04-10T18:47:55.0384893Z");
+	public static readonly DateTime BUILD_TIME = DateTime.Parse("2026-04-20T00:24:10.5214459Z");
 }
