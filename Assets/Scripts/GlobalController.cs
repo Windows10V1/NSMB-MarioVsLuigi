9241442af52c08415473371a12d265eda1faa9b4
Danny A <ipodtouch0218@gmail.com>
2026-05-18 21:43:03 -0400

    nightly v2.2.0.0 (5/18)

--- Assets/Scripts/GlobalController.cs
+++ Assets/Scripts/GlobalController.cs
@@ -1,7 +1,6 @@
 ﻿using NSMB.Addons;
 using NSMB.Networking;
 using NSMB.Quantum;
-using NSMB.Replay;
 using NSMB.Sound;
 using NSMB.UI;
 using NSMB.UI.Game;
@@ -13,15 +12,13 @@ using NSMB.Utilities.Extensions;
 using Quantum;
 using System;
 using System.Collections;
-using System.IO;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
-#if UNITY_STANDALONE
-using NSMB.UI.MainMenu.Submenus.Replays;
-using UnityEngine.Profiling;
+#if UNITY_STANDALONE && !UNITY_EDITOR 
+using NSMB.Replay;
+using UnityEngine.SceneManagement;
 #endif
 
 namespace NSMB {
@@ -29,16 +26,14 @@ namespace NSMB {
 
         //---Public Variables
         public TranslationManager translationManager;
-        public DiscordController discordController;
         public RumbleManager rumbleManager;
         public AnimatedFader fader;
         public AddonManager addonManager;
         public PauseOptionMenuManager optionsManager;
         public AudioMixerManager audioMixerManager;
-        public SimulationConfig config;
 
         public ScriptableRendererFeature outlineFeature;
-        public GameObject graphy, connecting;
+        public GameObject connecting;
         public LoadingCanvas loadingCanvas;
         public Image fullscreenFadeImage;
         public Sprite[] pingIndicators;
@@ -46,14 +41,11 @@ namespace NSMB {
 
         public PlayerSlotInfo[] playerSlots;
 
-        [NonSerialized] public bool checkedForVersion = false, firstConnection = true, bootedWithReplayArg;
+        [NonSerialized] public bool bootedWithReplayArg;
         [NonSerialized] public int windowWidth = 1280, windowHeight = 720;
 
         //---Private Variables
         private Coroutine totalAudioFadeRoutine;
-#if IDLE_LOCK_30FPS
-        private int previousVsyncCount, previousFrameRate;
-#endif
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         public static void CreateInstance() {
@@ -62,19 +54,15 @@ namespace NSMB {
 
         public void Awake() {
             Set(this);
-
-            firstConnection = true;
-            checkedForVersion = false;
         }
 
         public void Start() {
             AuthenticationHandler.IsAuthenticating = false;
-            Settings.Controls.Enable();
-            Settings.Controls.Debug.FPSMonitor.performed += ToggleFpsMonitor;
             QuantumEvent.Subscribe<EventStartGameEndFade>(this, OnStartGameEndFade);
             QuantumCallback.Subscribe<CallbackUnitySceneLoadDone>(this, OnUnitySceneLoadDone);
             loadingCanvas.Startup();
 
+#if UNITY_STANDALONE && !UNITY_EDITOR
             var commandLineArgs = Environment.GetCommandLineArgs();
             for (int i = 0; i < commandLineArgs.Length; i++) {
                 if (commandLineArgs[i] == "-replay" && commandLineArgs.Length > i + 1) {
@@ -82,94 +70,24 @@ namespace NSMB {
                     break;
                 }
             }
-        }
-
-        public void OnDestroy() {
-            Settings.Controls.Debug.FPSMonitor.performed -= ToggleFpsMonitor;
-            Settings.Controls.Disable();
+#endif
         }
 
         public void Update() {
             int newWindowWidth = Screen.width;
             int newWindowHeight = Screen.height;
 
-            var keyboard = Keyboard.current;
-
-            if (keyboard[Key.F3].wasPressedThisFrame) {
-                var game = QuantumRunner.DefaultGame;
-                if (game != null) {
-                    Frame f = game.Frames.Predicted;
-                    string dump = f.DumpFrame(Frame.DumpFlag_NoHeap);
-                    string path = $"{Application.persistentDataPath}/frame_dump_{DateTimeOffset.Now.ToUnixTimeMilliseconds()}.txt";
-                    Debug.Log($"[Debug] Created frame dump for frame {f.Number}- writing to {path}");
-                    File.WriteAllText(path, dump);
-                    PlaySound(SoundEffect.Player_Sound_PowerupCollect);
-                } else {
-                    PlaySound(SoundEffect.UI_Error);
-                }
-            }
-
-#if UNITY_STANDALONE
-    #if !UNITY_EDITOR
+#if UNITY_STANDALONE && !UNITY_EDITOR
             //todo: this jitters to hell
             if (Screen.fullScreenMode == FullScreenMode.Windowed && keyboard.leftShiftKey.isPressed && (windowWidth != newWindowWidth || windowHeight != newWindowHeight)) {
                 newWindowHeight = (int) (newWindowWidth * (9f / 16f));
                 Screen.SetResolution(newWindowWidth, newWindowHeight, FullScreenMode.Windowed);
             }
-    #endif
-
-
-            if (keyboard[Key.F6].wasPressedThisFrame && !string.IsNullOrEmpty(Application.consoleLogPath)) {
-                System.Diagnostics.Process.Start(Path.GetDirectoryName(Application.consoleLogPath));
-                PlaySound(SoundEffect.Player_Sound_PowerupCollect);
-            }
-
-            if (keyboard[Key.F7].wasPressedThisFrame && !string.IsNullOrEmpty(ReplayListManager.ReplayDirectory)) {
-                System.Diagnostics.Process.Start(ReplayListManager.ReplayDirectory);
-                PlaySound(SoundEffect.Player_Sound_PowerupCollect);
-            }
-            
-            if (keyboard[Key.F8].wasPressedThisFrame && addonManager.isActiveAndEnabled && !string.IsNullOrEmpty(AddonManager.LocalFolderPath)) {
-                System.Diagnostics.Process.Start(AddonManager.LocalFolderPath);
-                PlaySound(SoundEffect.Player_Sound_PowerupCollect);
-            }
-
-            if (Debug.isDebugBuild && keyboard[Key.F9].wasPressedThisFrame) {
-                if (Profiler.enabled) {
-                    Profiler.enabled = false;
-                    PlaySound(SoundEffect.Player_Sound_Powerdown);
-                } else {
-                    Profiler.maxUsedMemory = 256 * 1024 * 1024;
-                    Profiler.logFile = "profile-" + DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-                    Profiler.enableBinaryLog = true;
-                    Profiler.enabled = true;
-                    PlaySound(SoundEffect.Player_Sound_PowerupCollect);
-                }
-            }
 #endif
 
-            if (windowWidth != newWindowWidth || windowHeight != newWindowHeight) {
-                windowWidth = newWindowWidth;
-                windowHeight = newWindowHeight;
-            }
-
+            windowWidth = newWindowWidth;
+            windowHeight = newWindowHeight;
         }
-
-#if IDLE_LOCK_30FPS
-        public void OnApplicationFocus(bool focus) {
-            if (focus) {
-                QualitySettings.vSyncCount = previousVsyncCount;
-                Application.targetFrameRate = previousFrameRate;
-            } else {
-                // Lock framerate when losing focus to (hopefully) disable browsers slowing the game
-                previousVsyncCount = QualitySettings.vSyncCount;
-                previousFrameRate = Application.targetFrameRate;
-
-                QualitySettings.vSyncCount = 0;
-                Application.targetFrameRate = 30;
-            }
-        }
-#endif
 
         public void OnUnitySceneLoadDone(CallbackUnitySceneLoadDone e) {
             if (e.SceneName != null) {
@@ -203,15 +121,24 @@ namespace NSMB {
             }
         }
 
+#if UNITY_STANDALONE && !UNITY_EDITOR
         private void StartReplayFromArgs(string argReplayPath) {
             using FileStream input = new(argReplayPath, FileMode.Open);
             if (BinaryReplayFile.TryLoadNewFromStream(input, true, out var result) != ReplayParseResult.Success) {
-                Debug.LogError("Failed to parse replay file when booting with cmdline args...");
+                Debug.LogError("[Replay] Failed to parse replay file when booting with cmdline args...");
                 return;
             }
             bootedWithReplayArg = true;
+            NetworkHandler.OnError += StartReplayFromArgsErrorCallback;
             ActiveReplayManager.Instance.StartReplayPlayback(result);
         }
+
+        private void StartReplayFromArgsErrorCallback(string msg, bool networkError) {
+            bootedWithReplayArg = false;
+            SceneManager.LoadScene(0);
+            NetworkHandler.OnError -= StartReplayFromArgsErrorCallback;
+        }
+#endif
 
         private void OnStartGameEndFade(EventStartGameEndFade e) {
             if (MvLSceneLoader.Instance.CurrentLoadedMap != null) {
@@ -219,10 +146,6 @@ namespace NSMB {
                 StartCoroutine(FadeFullscreenImage(1, 1/3f));
                 totalAudioFadeRoutine = StartCoroutine(audioMixerManager.FadeOut(AudioMixerManager.KeyOverride));
             }
-        }
-
-        private void ToggleFpsMonitor(InputAction.CallbackContext obj) {
-            graphy.SetActive(!graphy.activeSelf);
         }
     }
 }
